08ff479ccebf824f88d117bb6b0b97b7384fda9a
unknown <leon_lira_@hotmail.com>
2020-01-09 15:18:40 -0600

    saf

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://localhost/crmdoc/web/backend/';
-$config['base_url'] = 'https://serverdoctor.admyo.com/';
+$config['base_url'] = 'https://doctor.admyo.com/backend/';
 /*
 |--------------------------------------------------------------------------
 | Index File
