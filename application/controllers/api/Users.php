58a6883e4973a5653147a7d44ba8132c8d852d1a
unknown <leon_lira_@hotmail.com>
2020-02-28 04:15:19 -0600

    sf

--- application/controllers/api/Users.php
+++ application/controllers/api/Users.php
@@ -174,7 +174,7 @@ class Users extends REST_Controller
             }
         }
     }
-
+    
     public function prueba_get(){
         print_r($this->authorization_token->userData());
         exit();
